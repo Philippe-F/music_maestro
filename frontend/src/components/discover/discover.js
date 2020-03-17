f32dacc427c843b7d1fbd943ab8118f6824ff9fc
Philippe Fonzin <pfonzin@gmail.com>
2020-03-17 17:45:26 -0400

    Create and sign in a user

---
+++ frontend/src/components/discover/discover.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Discover extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1> test </h1>
+    )
+  }
+}
