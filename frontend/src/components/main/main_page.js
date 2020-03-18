c8aa689fcd0ad80e3a73757fd9355cddd382ac29
nohani <nicole.takuhi@gmail.com>
2020-03-18 11:13:35 -0400

    splashpage

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import '../../styles/reset.css';
+import '../../styles/main.css';
 
 class MainPage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>A Twitter Clone</h1>
-        <footer>Copyright &copy; 2019 Chirper</footer>
+      <div className="responsive">
+        <div className="greeting"> 
+          <h1>SEE <strong>THE BEST</strong> CONCERTS IN YOUR AREA</h1>
+        </div>
+        <p className="description">See the most excting shows by today's most creative artists. Sign up to 
+          discover new music and shows.
+        </p>
+        <Link className="splash-pg-button" to='/signup'>Create Account</Link>
       </div>
     );
   }
