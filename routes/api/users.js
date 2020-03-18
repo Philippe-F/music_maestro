fb5dcce5a23fa218b3119746701c7c8d7d414d66
Philippe Fonzin <pfonzin@gmail.com>
2020-03-18 19:27:53 -0400

    Create user favorite route

--- routes/api/users.js
+++ routes/api/users.js
@@ -101,10 +101,17 @@ router.post("/register", (req, res) => {
 });
 
 router.get("/events", (req, res) => {
+  // get all events 
   Event.find()
   // pass in current user location when google maps api is finished 
   .sort({ date: -1 })
   .then(events => res.json(events))
+  .catch(err => res.status(400).json(err)) 
+});
+
+router.get("/users/user_id/my_favorites", (req, res) => {
+  User.find({id: req.params.user_id}) 
+  .then(user => res.json(user.favorites))
   .catch(err => res.status(400).json(err)) 
 });
 
