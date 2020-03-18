a5899cbbc17958453d6aad0d90a6cdca328bf03a
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-17 19:19:36 -0500

    git ignore node modules

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import "./navbar.css";
+import "../../stylesheets/nav.scss";
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -18,13 +18,14 @@ class NavBar extends React.Component {
   getLinks() {
     if (this.props.loggedIn) {
       return (
-        <div>
+        <div className="nav">
           <button onClick={this.logoutUser}>Logout</button>
         </div>
       );
     } else {
       return (
-        <div>
+        <div className="nav">
+          div
           <Link to={"/signup"}>Signup</Link>
           <Link to={"/login"}>Login</Link>
         </div>
