5e987260d039dbe72449f861b5b2e7b5200f3ae8
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-17 22:51:11 -0500

    Get nav bar in place and initial modal

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,6 +7,8 @@ import DiscoverContainer from './discover/discover_container'
 import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
+import '../stylesheets/reset.css'
+import "../stylesheets/nav.scss";
 
 const App = () => (
   <div>
@@ -15,7 +17,7 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
-      {/* <ProtectedRoute path="/discover" component={DiscoverContainer} /> */}
+      <ProtectedRoute path="/discover" component={DiscoverContainer} />
     </Switch>
   </div>
 );
