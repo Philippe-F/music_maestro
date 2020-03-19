d6767d087255d7eab0a8b4f45ca0b801c8bf9033
Philippe Fonzin <pfonzin@gmail.com>
2020-03-19 10:45:51 -0400

    User thunk action creators

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -11,3 +11,27 @@ export const receiveUserData = data => ({
   type: RECEIVE_ALL_DATA,
   data: data
 });
+
+export const fetchEvents = () => dispatch => (
+  APIUtil.getEvents()
+    .then(events => dispatch(receiveAllEvents(events)))
+    .catch(err => console.log(err))
+);
+
+export const fetchUserFavorites = userId => dispatch => (
+  APIUtil.getUserFavorites(userId)
+    .then(events => dispatch(receiveUserData(events)))
+    .catch(err => console.log(err))
+);
+
+export const fetchUserArtists = userId => dispatch => (
+  APIUtil.getUserArtists(userId)
+    .then(artists => dispatch(receiveUserData(artists)))
+    .catch(err => console.log(err))
+);
+
+export const fetchUserVenues = userId => dispatch => (
+  APIUtil.getUserVenues(userId)
+    .then(venues => dispatch(receiveUserData(venues)))
+    .catch(err => console.log(err))
+);
