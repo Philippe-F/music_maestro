6fbbcf54ce72fdf0268efee55da2c8ae610e83f3
Philippe Fonzin <pfonzin@gmail.com>
2020-03-19 14:17:31 -0400

    Users reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import session from "./session_reducer";
 import errorsReducer from "./errors_reducer"
+import userReducer from "./users_reducer";
 
 const RootReducer = combineReducers({
   session: session,
-  errors: errorsReducer
+  errors: errorsReducer, 
+  user: userReducer
 });
 
 export default RootReducer;
