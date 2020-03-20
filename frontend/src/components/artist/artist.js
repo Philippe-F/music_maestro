ddb2cf7b4cfbdd320e2b86267f0e7f2d7da78ffe
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-20 07:42:12 -0500

    Initialize artist show component and container

---
+++ frontend/src/components/artist/artist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Artist extends React.Component {
+
+  render() {
+    return (
+      <div className="responsive">
+
+      </div>
+    )
+  }
+}
+
+export default Artist
