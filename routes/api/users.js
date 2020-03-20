c89f13548150641457d65afe7c7a79f887f2a600
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-20 16:11:15 -0500

    Commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,7 +17,6 @@ router.get('/current', passport.authenticate('jwt', {session: false}), (req, res
   });
 })
 
-
 router.post("/login", (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
 
@@ -126,6 +125,14 @@ router.get("/users/user_id/my_venues", (req, res) => {
   User.find({id: req.params.user_id}) 
   .then(user => res.json(user.follows.venues))
   .catch(err => res.status(400).json(err)) 
+});
+
+router.post("/", singleMulterUpload("image"), async (req, res) => {
+  const userData = req.body;
+  userData.image = await singlePublicFileUpload(req.file);
+  const user = new User(userData);
+  await user.save();
+  res.json(user);
 });
 
 
