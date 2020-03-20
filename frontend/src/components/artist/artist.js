59c2b4a727c032bbb0cbfeee50ce2a4969f664e8
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-20 08:03:44 -0500

    Work on artist component

--- frontend/src/components/artist/artist.js
+++ frontend/src/components/artist/artist.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import DiscoverItem from '../discover/discover_item'
 class Artist extends React.Component {
 
   render() {
     return (
       <div className="responsive">
-
+        <div className="discover-wrapper">
+          <div className="artist">
+            <h1 className="discover-header">Artist Component</h1>
+            <div className="discover-collection">
+              <DiscoverItem />
+              <DiscoverItem />
+              <DiscoverItem />
+              <DiscoverItem />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
