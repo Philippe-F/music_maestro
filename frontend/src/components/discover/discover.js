f5743141ff9ff959353d54ffdc84490852a9b13d
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-22 15:39:38 -0500

    Create functions to filter events based on user follows

--- frontend/src/components/discover/discover.js
+++ frontend/src/components/discover/discover.js
@@ -4,15 +4,41 @@ import DiscoverItem from './discover_item'
 export default class Discover extends React.Component{
   constructor(props) {
     super(props)
-
+    this.aggregateEvents = this.aggregateEvents.bind(this)
+    this.userEvents = this.userEvents.bind(this)
+    this.allEvents = this.allEvents.bind(this)
   }
 
   componentDidMount() {
-    this.props.fetchEvents()
+    const { id } = this.props.user
+    this.props.fetchEvents().then()
+    this.props.fetchUserArtists(id)
+    this.props.fetchUserVenues(id)
   }
 
   aggregateEvents() {
-    
+    let events = null;
+    let artists = null;
+    let venues = null;
+    let userEvents = {};
+    if (this.props.events && this.props.venues && this.props.artists) {
+      artists = this.props.artists.data.map((artist) => {
+        return artist._id
+      })
+      venues = this.props.venues.data.map((venue) => {
+        return venue._id
+      })
+      events = this.props.events.data.map((event) => {
+        if (artists.includes(event.artists[0]._id)) {
+          userEvents[event._id] = event
+        } else if (venues.includes(event.venue._id)) {
+          userEvents[event._id] = event
+        }
+      })
+      return userEvents
+    } else {
+      return null
+    }
   }
 
   allEvents() {
@@ -29,16 +55,30 @@ export default class Discover extends React.Component{
   }
 
   userEvents() {
-    let events = null
+    const userE = this.aggregateEvents()
+    if (userE) {
+      const events = Object.values(userE)
+      const discoverEvents = events.map((event) => {
+        return (
+          <DiscoverItem key={event._id} event={event} />
+        )
+      })
+      return discoverEvents
+    } else {
+      return null
+    }
   }
 
   render() {
-    this.props.events ? console.log(this.props.events.data) : console.log("hello")
     return (
       <div className="responsive">
         <div className="discover-wrapper">
           <div className="discover">
             <h1 className="discover-header">JUST FOR YOU</h1>
+            <div className="discover-collection">
+              {this.userEvents()}
+            </div>
+            <h1 className="discover-header">HAPPENING SOON</h1>
             <div className="discover-collection">
               {this.allEvents()}
             </div>
