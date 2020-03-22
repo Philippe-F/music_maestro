de08929828e5420440fa8fa56bde1fce7dba7c76
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-22 17:35:22 -0500

    Fill out event container and component before testing

--- frontend/src/components/event/event_show_container.js
+++ frontend/src/components/event/event_show_container.js
@@ -1,21 +1,25 @@
 import { connect } from 'react-redux'
 import Event from './event_show'
-import {fetchEvent} from '../../actions/event_actions'
+import {fetchEvents} from '../../actions/user_actions'
+import { favoriteEvent, unfavoriteEvent } from '../../actions/fav_and_follow_actions'
 
 const mSTP = (state, ownProps) => {
   let eventProp = null
   if (state.events.data) {
-    eventProp = state.events.data.map((e) => {
-      if (e._id === ownProps.eventId) return e
+    state.events.data.map((e) => {
+      if (e._id === ownProps.match.params.eventId) eventProp = e
     })
   }
   return {
-    event: eventProp
+    event: eventProp,
+    currentUser: state.session.user
   }
 }
 
 const mDTP = dispatch => ({
-  fetchEvent: (eventId) => dispatch(fetchEvent(eventId))
+  fetchEvents: () => dispatch(fetchEvents()),
+  favoriteEvent: (userId, eventId) => dispatch(favoriteEvent(userId, eventId)),
+  unfavoriteEvent: (userId, eventId) => dispatch(unfavoriteEvent(userId, eventId)),
 })
 
 export default connect(mSTP, mDTP)(Event)
