8e463e3b80b99f59330e6a041b1278e220e9bd05
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-27 10:38:35 -0500

    Finish close button

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -5,6 +5,7 @@ export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
+export const RECEIVE_CLEAR_ERRORS = "RECEIVE_CLEAR_ERRORS";
 
 // We'll dispatch this when our user signs in
 export const receiveCurrentUser = currentUser => ({
@@ -61,3 +62,7 @@ export const logout = () => dispatch => {
   APIUtil.setAuthToken(false);
   dispatch(logoutUser());
 };
+
+export const clearErrors = () => ({
+  type: RECEIVE_CLEAR_ERRORS
+})
--- frontend/src/components/errors.js
+++ frontend/src/components/errors.js
@@ -1,27 +1,52 @@
 import { connect } from "react-redux"
 import React from "react"
+import { clearErrors } from '../actions/session_actions'
 
-const Errors = (props) => {
-  let errorMessages = props.errors
-  let slide = ""
-  if (props.errors.length > 0) slide = "slide-in"
-  if (props.errors.length > 2) errorMessages = ["Looks like you missed some things"]
-  let errs = errorMessages.map(
-    (err, i) => <li className="message" key={i}>{err}</li>
-  )
-  return (
-    <div className={`err-flash-messages ${slide}`}>
-      <div className="error-content">
-        <ul className="message-list">
-          {errs}
-        </ul>
+class Errors extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      slide: "slide-in",
+      errCount: 0
+    }
+  }
+
+  closeErrors() {
+    return () => (
+      this.props.clearErrors()
+    )
+  }
+
+  render() {
+    let slide = null
+    console.log(slide)
+    let errorMessages = this.props.errors
+    if (this.props.errors.length > 0) slide = this.state.slide
+    if (this.props.errors.length > 2) errorMessages = ["Looks like you missed some things"]
+    let errs = errorMessages.map(
+      (err, i) => <li className="message" key={i}>{err}</li>
+    )
+    return (
+      <div className={`err-flash-messages ${slide}`}>
+        <div className="error-content">
+          <ul className="message-list">
+            {errs}
+          </ul>
+          <button className="dismiss" onClick={this.closeErrors()}>
+            <i className="fas fa-times close-icon"></i>
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mSTP = state => ({
   errors: Object.values(state.errors.session)
 })
 
-export default connect(mSTP, {})(Errors)
+const mDTP = dispatch => ({
+  clearErrors: () => dispatch(clearErrors())
+})
+
+export default connect(mSTP, mDTP)(Errors)
