92a72554d0274cb8ebdcebcda4ae7ebe8f63a2bf
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-02 16:11:17 -0500

    Remove console log in errors

--- frontend/src/components/errors.js
+++ frontend/src/components/errors.js
@@ -1,52 +1,50 @@
-import { connect } from "react-redux"
-import React from "react"
-import { clearErrors } from '../actions/session_actions'
+import { connect } from "react-redux";
+import React from "react";
+import { clearErrors } from "../actions/session_actions";
 
 class Errors extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       slide: "slide-in",
-      errCount: 0
-    }
+      errCount: 0,
+    };
   }
 
   closeErrors() {
-    return () => (
-      this.props.clearErrors()
-    )
+    return () => this.props.clearErrors();
   }
 
   render() {
-    let slide = null
-    console.log(slide)
-    let errorMessages = this.props.errors
-    if (this.props.errors.length > 0) slide = this.state.slide
-    if (this.props.errors.length > 2) errorMessages = ["Looks like you missed some things"]
-    let errs = errorMessages.map(
-      (err, i) => <li className="message" key={i}>{err}</li>
-    )
+    let slide = null;
+    let errorMessages = this.props.errors;
+    if (this.props.errors.length > 0) slide = this.state.slide;
+    if (this.props.errors.length > 2)
+      errorMessages = ["Looks like you missed some things"];
+    let errs = errorMessages.map((err, i) => (
+      <li className="message" key={i}>
+        {err}
+      </li>
+    ));
     return (
       <div className={`err-flash-messages ${slide}`}>
         <div className="error-content">
-          <ul className="message-list">
-            {errs}
-          </ul>
+          <ul className="message-list">{errs}</ul>
           <button className="dismiss" onClick={this.closeErrors()}>
             <i className="fas fa-times close-icon"></i>
           </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mSTP = state => ({
-  errors: Object.values(state.errors.session)
-})
+const mSTP = (state) => ({
+  errors: Object.values(state.errors.session),
+});
 
-const mDTP = dispatch => ({
-  clearErrors: () => dispatch(clearErrors())
-})
+const mDTP = (dispatch) => ({
+  clearErrors: () => dispatch(clearErrors()),
+});
 
-export default connect(mSTP, mDTP)(Errors)
+export default connect(mSTP, mDTP)(Errors);
