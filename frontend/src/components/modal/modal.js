b5d8e35ed424dc831e78048b766f1bd41e81671c
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-03 14:44:39 -0500

    Make mobile tab divs buttons

--- frontend/src/components/modal/modal.js
+++ frontend/src/components/modal/modal.js
@@ -1,64 +1,77 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import LoginFormContainer from '../session/login_form_container'
-import SignupFormContainer from '../session/signup_form_container'
-import { openModal, closeModal } from '../../actions/modal_actions'
-
-
+import React from "react";
+import { connect } from "react-redux";
+import LoginFormContainer from "../session/login_form_container";
+import SignupFormContainer from "../session/signup_form_container";
+import { openModal, closeModal } from "../../actions/modal_actions";
 
 class Modal extends React.Component {
-
   render() {
-    const {modal, closeModal} = this.props
+    const { modal, closeModal } = this.props;
     if (!modal) {
-      return null
+      return null;
     }
     let activeSignIn = null;
     let activeSignUp = null;
     let component = null;
     switch (modal) {
       case "login":
-        component = <LoginFormContainer />
-        activeSignIn = "active"  
-        break
+        component = <LoginFormContainer />;
+        activeSignIn = "active";
+        break;
       case "signup":
-        component = <SignupFormContainer />
-        activeSignUp = "active"
-        break
+        component = <SignupFormContainer />;
+        activeSignUp = "active";
+        break;
       default:
-        return null
+        return null;
     }
 
     return (
       <div onClick={closeModal} className="overlay">
         <div className="content-wrapper">
           <div className="content">
-            <button onClick={this.openUserModal} className={`modal-close-button`}>
+            {/* <button
+              onClick={this.openUserModal}
+              className={`modal-close-button`}
+            >
               <i className="fas fa-times"></i>
-            </button>
+            </button> */}
             <div className="authentication">
               <div className="mobile-tabs">
-                <button className={activeSignIn} >Sign In</button>
-                <button className={activeSignUp} >Create Account</button>
+                <button
+                  className={activeSignIn}
+                  onClick={() => this.props.openModal("login")}
+                >
+                  Sign In
+                </button>
+                <button
+                  className={activeSignUp}
+                  onClick={() => this.props.openModal("login")}
+                >
+                  Create Account
+                </button>
               </div>
-              <div onClick={e => e.stopPropagation()} className="auth-container">
-                { component }
+              <div
+                onClick={(e) => e.stopPropagation()}
+                className="auth-container"
+              >
+                {component}
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mSTP = state => ({
-  modal: state.ui.modal
-})
+const mSTP = (state) => ({
+  modal: state.ui.modal,
+});
 
-const mDTP = dispatch => ({
+const mDTP = (dispatch) => ({
   closeModal: () => dispatch(closeModal()),
-  openModal: (modal) => dispatch(openModal(modal))
-})
+  openModal: (modal) => dispatch(openModal(modal)),
+});
 
-export default connect(mSTP, mDTP)(Modal)
+export default connect(mSTP, mDTP)(Modal);
