d81ddbd75bf8bf0c1a313111baf749088df90393
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-04 13:23:48 -0500

    Render favs on initial log in

--- frontend/src/components/discover/discover_container.js
+++ frontend/src/components/discover/discover_container.js
@@ -4,12 +4,14 @@ import {
   fetchEvents,
   fetchUserArtists,
   fetchUserVenues,
+  fetchUserFavorites,
 } from "../../actions/user_actions";
 
 const mSTP = (state) => {
   return {
     events: state.user.events,
     user: state.user,
+    id: state.session.user.id,
     artists: state.user.userArtists,
     venues: state.user.userVenues,
   };
@@ -19,6 +21,7 @@ const mDTP = (dispatch) => ({
   fetchEvents: () => dispatch(fetchEvents()),
   fetchUserArtists: (userId) => dispatch(fetchUserArtists(userId)),
   fetchUserVenues: (userId) => dispatch(fetchUserVenues(userId)),
+  fetchUserFavorites: (userId) => dispatch(fetchUserFavorites(userId)),
 });
 
 export default connect(mSTP, mDTP)(Discover);
