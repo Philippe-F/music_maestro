b5836ebb9c397c1b5ff008415f637d47002a6f21
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-05 18:21:16 -0500

    Add image and blurb to splash page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -18,6 +18,23 @@ class MainPage extends React.Component {
             See the most excting shows by today's most creative artists. Sign up
             to discover new music and shows.
           </p>
+          <div className="splash-img-container">
+            <img
+              className="splash-img"
+              src="https://musicmaestro-seed.s3.us-east-2.amazonaws.com/outdoor.jpg"
+              alt=""
+            />
+          </div>
+          <p className="description lg">
+            Wanna see the stuff happening soon? Scroll down.
+            <br />
+            Looking for something in particular? Click on the magnifying glass
+            to search for shows and concerts by artist name, venue, or tour
+            name.
+            <br />
+            See something you like and don't want to forget it? Click on an
+            event, then click the favorite button.
+          </p>
           <div
             className="splash-pg-button"
             onClick={() => this.props.openModal("login")}
