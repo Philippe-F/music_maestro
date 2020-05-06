79eb810b21306e173d219192f32913abb2f97ccd
Philippe Fonzin <pfonzin@gmail.com>
2020-05-05 21:54:33 -0400

    Write follow function

--- frontend/src/components/artist/artist.js
+++ frontend/src/components/artist/artist.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import DiscoverItem from '../discover/discover_item'
+import React from 'react';
+
 class Artist extends React.Component {
   constructor(props) {
     super(props);
@@ -27,25 +27,79 @@ class Artist extends React.Component {
     return names;
   }
 
-  // getArtistDetails() {
-  //   let artists = this.getArtist();
-  //   let artistId = this.props.artistId;
-    
-  //   for (let i = 0; i < artists.length; i++) {
-  //     let artist = artists[i];
-  //     if (artistId === artist._id) {
-  //       return <DiscoverItem key={artist._id} name={artist.name} image={artist.img} />;
-  //       break
-  //     } else {
-  //       continue
-  //     }
-  //   }
-  // }
+  getArtistId() {
+    let artists = this.getArtist();
+    let artistId = this.props.artistId;
+    for (let i = 0; i < artists.length; i++) {
+      let artist = artists[i];
+      if (artistId === artist._id) {
+        return artist._id;
+      } 
+    }
+  }
+  
+  getArtistName() {
+    let artists = this.getArtist();
+    let artistId = this.props.artistId;
+    for (let i = 0; i < artists.length; i++) {
+      let artist = artists[i];
+      if (artistId === artist._id) {
+        return artist.name;
+      } 
+    }
+  }
+
+  getArtistImage() {
+    let artists = this.getArtist();
+    let artistId = this.props.artistId;
+    for (let i = 0; i < artists.length; i++) {
+      let artist = artists[i];
+      if (artistId === artist._id) {
+        return artist.img;
+      } 
+    }
+  }
+
+  handleFavorite() {
+    const userId = this.props.currentUser.id;
+    const artistId = this.getArtistId();
+    const favIds = this.props.favorites.map((fav) => {
+      //there is no follows action
+      return fav._id;
+    });
+    if (this.props.favorites.includes(artistId) || favIds.includes(artistId)) {
+      this.props.unfollowArtist(userId, artistId);
+    } else {
+      this.props.followArtist(userId, artistId);
+    }
+  }
+
+  star() {
+    const eventId = this.props.event._id;
+    const favIds = this.props.favorites.map((fav) => {
+      return fav._id;
+    });
+    if (this.props.favorites.includes(eventId) || favIds.includes(eventId)) {
+      return (
+        <div className="img-wrapper">
+          <i className="fas fa-star" id="artist-star"></i>
+        </div>
+      );
+    } else {
+      return (
+        <div className="img-wrapper">
+          <i className="far fa-star" id="artist-star"></i>
+        </div>
+      );
+    }
+  }
 
   render() {
-    const arr = this.getArtistDetails();
+    const name = this.getArtistName();
+    const id= this.getArtistId();
+    const image= this.getArtistImage();
 
-    console.log("names", arr)
+    console.log("names", image)
     return (
           <div className="responsive">
             <div className="discover-wrapper">
@@ -53,11 +107,11 @@ class Artist extends React.Component {
                 <div className="artist-metadata">
                   <div className="artist-poster">
                     {/* image tag goes here className="artist-image" */}
-                    <DiscoverItem/>
+                    <img src={image} />
                   </div>
                   <div className="text-wrapper">
                     <div className="artist-info">
-                      Artist Name
+                      <h2>{name}</h2>
                     </div>
                     <div className="action-buttons">
                       <button className="form-button action-button">
@@ -69,7 +123,7 @@ class Artist extends React.Component {
                     </div>
                   </div>
                 </div>
-                <h1 className="discover-header">Artist Component</h1>
+                <h1 className="discover-header">Upcoming Shows</h1>
                 <div className="discover-collection">
                   {/* <DiscoverItem />
                   <DiscoverItem />
--- frontend/src/components/artist/artist_container.js
+++ frontend/src/components/artist/artist_container.js
@@ -1,11 +1,14 @@
-import { connect } from 'react-redux'
-import Artist from './artist'
+import { connect } from 'react-redux';
+import Artist from './artist';
 import { fetchEvents } from "../../actions/user_actions";
+import { followArtist, unfollowArtist } from "../../actions/fav_and_follow_actions";
 
 const mSTP = (state, ownProps) => {
   const artistId = ownProps.match.params.artistId;
 
   return {
+    favorites: state.user.userFavorites,
+    currentUser: state.session.user,
     errors: state.errors.session,
     artistId: artistId,
     events: state.events.data,
@@ -13,7 +16,9 @@ const mSTP = (state, ownProps) => {
 };
 
 const mDTP = dispatch => ({
-  fetchEvents: () => dispatch(fetchEvents())
+  fetchEvents: () => dispatch(fetchEvents()),
+  followArtist: (userId, artistId) => dispatch(followArtist(userId, artistId)),
+  unfollowArtist: (userId, artistId) => dispatch(followArtist(userId, artistId))
 });
 
 export default connect(mSTP, mDTP)(Artist);
