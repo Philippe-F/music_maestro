3eeb294684bcdaad88c0ac5dc1c2af3abf49fb42
Philippe Fonzin <pfonzin@gmail.com>
2020-05-06 08:16:29 -0400

    Follow actions

--- frontend/src/actions/fav_and_follow_actions.js
+++ frontend/src/actions/fav_and_follow_actions.js
@@ -3,6 +3,7 @@ import * as FavAPIUtil from "../util/favorites_util";
 
 export const RECEIVE_USER = "RECEIVE_USER"
 export const UPDATE_USER_FAVORITES = "UPDATE_USER_FAVORITES"
+export const UPDATE_USER_FOLLOWS = "UPDATE_USER_FOLLOWS"
 
 const receiveUser = user => ({
   type: RECEIVE_USER,
@@ -14,6 +15,10 @@ const updateUserFavorites = data => ({
   data
 })
 
+const updateUserFollows = data => ({
+  type: UPDATE_USER_FOLLOWS,
+  data
+})
 
 
 export const followVenue = (userId, venueId) => dispatch => FollowAPIUtil.followVenue(userId, venueId)
@@ -35,5 +40,11 @@ export const favoriteEvent = (userId, eventId) => dispatch => FavAPIUtil.favorit
 
  export const unfavoriteEvent = (userId, eventId) => dispatch => FavAPIUtil.unfavoriteEvent(userId, eventId)
   .then((user => dispatch(updateUserFavorites(user))));
+
+  export const unfollowArtist = (userId, artistId) => dispatch => FollowAPIUtil.unfollowArtist(userId, artistId)
+  .then((user => dispatch(updateUserFollows(user))));
+
+  export const followArtist = (userId, artistId) => dispatch => FollowAPIUtil.followArtist(userId, artistId)
+  .then((user => dispatch(updateUserFollows(user))));
 
 
